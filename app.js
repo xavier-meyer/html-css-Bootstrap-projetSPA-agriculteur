06837da3d13990c5607d8f38f008805427596db5
xavier-meyer <xavier.01@orange.fr>
2022-03-19 14:56:37 +0100

    feat: back-to-top

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const btn = document.querySelector(".btn-arrow");
-btn.addEventListener("click", () => {
+// déclaration du bouton
+btnReturn = document.getElementById("btn-return");
 
-    Window.scrollTo({
-        top: 0,
-        left : 0,
-        behavior: "smooth"
-    })
-})
-  
+// montrer le bouton si on scrolle de 100 px
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 180 || document.documentElement.scrollTop > 180){
+        btnReturn.style.display = "block";
+    }else{
+        btnReturn.style.display = "none";
+    }
+}
+// fonction goToTop
+function goToTop(){
+    // fct scrollIntoView() => faire défiler la page pour afficher l'élément auquel elle est appellée.
+    // ici: nous l'appellons sur le body afin que la page défile vers le haut.
+    document.body.scrollIntoView({
+        behavior : "smooth",
+    });
+}
+// appel goToTop() au clic du bouton de retour en haut
+
+btnReturn.addEventListener("click", goToTop);
